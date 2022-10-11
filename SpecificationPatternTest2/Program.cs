5d1d3ccaf8277c65ab0604d0027ad505352db297
Benjamin <beela93@hotmail.com>
2022-10-11 12:24:49 +0200

    partly implemented composite based upon the wiki page

---
+++ SpecificationPatternTest2/Program.cs
@@ -0,0 +1,37 @@
+﻿using SpecificationPatternTest2.SpecificationPattern;
+
+var rule = new TestSpecifiction().And(new Under10TestSpecificaation()).Or(new Over4Specification());
+//read more into https://www.martinfowler.com/apsupp/spec.pdf and figure out how to best implementing some of the things that are mentioned
+var test = rule.IsSatisfiedBy(7);
+
+
+Console.WriteLine(test);
+
+
+
+
+public class TestSpecifiction : CompositeSpecification<int>
+{
+    private int _test = 5;
+    public override bool IsSatisfiedBy(int candidate)
+    {
+        return candidate != _test;
+    }
+}
+
+public class Under10TestSpecificaation : CompositeSpecification<int>
+{
+    public override bool IsSatisfiedBy(int candidate)
+    {
+        return candidate < 10;
+    }
+}
+
+public class Over4Specification : CompositeSpecification<int>
+{
+    public override bool IsSatisfiedBy(int candidate)
+    {
+        return candidate > 4;
+    }
+}
+
---
+++ SpecificationPatternTest2/SpecificationPattern/ISpecification.cs
@@ -0,0 +1,76 @@
+﻿namespace SpecificationPatternTest2.SpecificationPattern
+{
+    public interface ISpecification<T>
+    { //based upon the wikipedia page.
+        bool IsSatisfiedBy(T candidate);
+        ISpecification<T> And(ISpecification<T> other);
+        ISpecification<T> AndNot(ISpecification<T> other);
+        ISpecification<T> Or(ISpecification<T> other);
+        ISpecification<T> OrNot(ISpecification<T> other);
+        ISpecification<T> Not();
+    }
+
+    public abstract class CompositeSpecification<T> : ISpecification<T>
+    {
+        public abstract bool IsSatisfiedBy(T candidate);
+
+        public ISpecification<T> And(ISpecification<T> other)
+        {
+            return new AndSpecification<T>(this, other);
+        }
+
+        public ISpecification<T> AndNot(ISpecification<T> other)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ISpecification<T> Not()
+        {
+            throw new NotImplementedException();
+        }
+
+        public ISpecification<T> Or(ISpecification<T> other)
+        {
+            return new OrSpecification<T>(this, other);
+        }
+
+        public ISpecification<T> OrNot(ISpecification<T> other)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class AndSpecification<T> : CompositeSpecification<T>
+    {
+        ISpecification<T> _left;
+        ISpecification<T> _right;
+
+        public AndSpecification(ISpecification<T> left, ISpecification<T> right)
+        {
+            _left = left;
+            _right = right;
+        }
+
+        public override bool IsSatisfiedBy(T candidate)
+        {
+            return _left.IsSatisfiedBy(candidate) && _right.IsSatisfiedBy(candidate);
+        }
+    }
+
+    public class OrSpecification<T> : CompositeSpecification<T>
+    {
+        ISpecification<T> _left;
+        ISpecification<T> _right;
+
+        public OrSpecification(ISpecification<T> left, ISpecification<T> right)
+        {
+            _left = left;
+            _right = right;
+        }
+
+        public override bool IsSatisfiedBy(T candidate)
+        {
+            return _left.IsSatisfiedBy(candidate) || _right.IsSatisfiedBy(candidate);
+        }
+    }
+}
